5dbb9eb9f42a6ffc96ceff5c2abab22e1da0859c
hemel17 <redewanmahmud@gmail.com>
2024-06-30 11:54:16 +0600

    delete func added

--- index.js
+++ index.js
@@ -52,6 +52,15 @@ async function run() {
       res.send(result);
     });
 
+    // delete a doucment
+    app.delete("/forest/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await forestCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
